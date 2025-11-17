b1548466e6c9824ea586766c1b3fa37be72e1854
pomazkovanton <vitrumbeta@gmail.com>
2025-11-17 19:26:58 +0800

    chore(git-hooks): configure pre-commit and commit-msg hooks #2

---
+++ git-hooks.config.ts
@@ -0,0 +1,14 @@
+const config = {
+  'pre-commit': {
+    stagedLint: {
+      '*.{js,jsx,ts,tsx,mjs,cjs}': ['bun lint:fix', 'bun format'],
+      '*.{json,md,yml,yaml}': 'prettier --write',
+      '*.{css,scss,html}': 'prettier --write',
+    },
+  },
+
+  'commit-msg': 'bunx --no -- commitlint --edit $1',
+  verbose: true,
+};
+
+export default config;
