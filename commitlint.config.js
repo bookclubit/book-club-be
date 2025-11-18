06ea3ab52fb818e830833b3abdf57fc63c6e9128
pomazkovanton <vitrumbeta@gmail.com>
2025-11-18 19:14:03 +0800

    feat: добавить правило проверки номера задачи #2

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+
+  parserPreset: {
+    parserOpts: {
+      headerPattern: /^(\w+)(?:\(([^)]+)\))?: (.+) #(\d+)$/,
+      headerCorrespondence: ['type', 'scope', 'subject', 'ticket'],
+    },
+  },
+
   rules: {
     'type-enum': [
       2,
@@ -19,5 +27,7 @@ module.exports = {
     ],
     'subject-min-length': [2, 'always', 3],
     'header-max-length': [2, 'always', 100],
+    'type-empty': [2, 'never'],
+    'subject-empty': [2, 'never'],
   },
 };
